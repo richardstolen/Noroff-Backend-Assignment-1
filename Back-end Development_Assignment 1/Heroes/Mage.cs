8e0abb31de0abe90846fd896f361ff15b4a05e2d
richardstolen <richstolen@gmail.com>
2023-01-12 15:29:04 +0100

    feat: Started with Mage hero implementation

---
+++ Back-end Development_Assignment 1/Heroes/Mage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Back_end_Development_Assignment_1.Heroes
+{
+    public class Mage : Hero
+    {
+        public Mage(string name) : base(name)
+        {
+            LevelAttributes = new HeroAttribute(1, 1, 6);
+        }
+
+        public override void levelUp()
+        {
+            base.levelUp();
+            LevelAttributes.levelUp(1, 1, 5);
+        }
+
+        public override void damage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void displayHero()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void totalAttributes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
