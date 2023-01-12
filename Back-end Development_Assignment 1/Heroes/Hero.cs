33ed27fe71f922de541837f784b072fadc3175bb
richardstolen <richstolen@gmail.com>
2023-01-12 15:28:26 +0100

    feat: Added Hero and Heroattribute class

---
+++ Back-end Development_Assignment 1/Heroes/Hero.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Back_end_Development_Assignment_1
+{
+    public abstract class Hero
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public HeroAttribute LevelAttributes { get; set; }
+        public List<Item> Items { get; set; }
+        public List<WeaponType> ValidWeaponTypes { get; set; }
+        public List<ArmorType> ValidArmorTypes { get; set; }
+
+        public Hero(string name)
+        {
+            Name = name;
+            Level = 1;
+        }
+
+        public virtual void levelUp()
+        {
+            Level++;
+        }
+
+        public void equipWeapon(Weapon weapon)
+        {
+            Items.Add(weapon);
+        }
+
+        public void equipArmor(Armor armor)
+        {
+            Items.Add(armor);
+        }
+
+        public abstract void damage();
+        public abstract void totalAttributes();
+        public abstract void displayHero();
+    }
+}
---
+++ Back-end Development_Assignment 1/Heroes/HeroAttribute.cs
@@ -0,0 +1,30 @@
+﻿namespace Back_end_Development_Assignment_1
+{
+    public class HeroAttribute
+    {
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Intelligence { get; set; }
+
+        public HeroAttribute()
+        {
+            Strength = 1;
+            Dexterity = 1;
+            Intelligence = 1;
+        }
+
+        public HeroAttribute(int strength, int dexterity, int intelligence)
+        {
+            Strength = strength;
+            Dexterity = dexterity;
+            Intelligence = intelligence;
+        }
+
+        public void levelUp(int strength, int dexterity, int intelligence)
+        {
+            Strength += strength;
+            Dexterity += dexterity;
+            Intelligence += intelligence;
+        }
+    }
+}
