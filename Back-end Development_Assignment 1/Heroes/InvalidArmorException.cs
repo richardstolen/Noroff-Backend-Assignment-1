51dd4fc915299948baa9863b05430596e3a8c930
richardstolen <richstolen@gmail.com>
2023-01-13 14:47:52 +0100

    feat: Custom invalid armor exception

---
+++ Back-end Development_Assignment 1/Heroes/InvalidArmorException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Back_end_Development_Assignment_1
+{
+    [Serializable]
+    public class InvalidArmorException : Exception
+    {
+        public InvalidArmorException()
+        {
+        }
+
+        public InvalidArmorException(string message) : base(message)
+        {
+        }
+
+        public InvalidArmorException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidArmorException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
