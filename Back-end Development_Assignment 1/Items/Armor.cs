bdb4efdf0204d6a7b2167a5e5df795a9e71daa53
richardstolen <richstolen@gmail.com>
2023-01-13 14:48:26 +0100

    feat: Started on armor and armorattribute implementation

--- Back-end Development_Assignment 1/Items/Armor.cs
+++ Back-end Development_Assignment 1/Items/Armor.cs
@@ -1,6 +1,16 @@
-﻿namespace Back_end_Development_Assignment_1
+﻿using Back_end_Development_Assignment_1.Items;
+
+namespace Back_end_Development_Assignment_1
 {
     public class Armor : Item
     {
+        public ArmorType ArmorType { get; set; }
+        public ArmorAttribute ArmorAttribute { get; set; }
+
+        public Armor(string name, int requiredLevel, Slot slot, ArmorType armorType, ArmorAttribute armorAttribute) : base(name, requiredLevel, slot)
+        {
+            ArmorType = armorType;
+            ArmorAttribute = armorAttribute;
+        }
     }
 }
---
+++ Back-end Development_Assignment 1/Items/ArmorAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Back_end_Development_Assignment_1.Items
+{
+    public class ArmorAttribute
+    {
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Intelligence { get; set; }
+
+        public ArmorAttribute()
+        {
+            Strength = 1;
+            Dexterity = 1;
+            Intelligence = 1;
+        }
+
+        public ArmorAttribute(int strength, int dexterity, int intelligence)
+        {
+            Strength = strength;
+            Dexterity = dexterity;
+            Intelligence = intelligence;
+        }
+    }
+}
