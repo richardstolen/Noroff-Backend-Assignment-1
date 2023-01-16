f2f2d6963658d519168b27a50ee7c3f3703930fe
richardstolen <richstolen@gmail.com>
2023-01-16 13:02:07 +0100

    feat: Added Ranger class

---
+++ Back-end Development_Assignment 1/Heroes/Ranger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Back_end_Development_Assignment_1.Heroes
+{
+    public class Ranger : Hero
+    {
+        public Ranger(string name) : base(name)
+        {
+            LevelAttributes = new HeroAttribute(1, 7, 1);
+            setValidArmorTypes(new List<ArmorType>() { ArmorType.Leather, ArmorType.Mail });
+            setValidWeaponTypes(new List<WeaponType>() { WeaponType.Bows, WeaponType.Unarmed });
+        }
+
+        public override void levelUp()
+        {
+            base.levelUp();
+            LevelAttributes.levelUp(1, 5, 1);
+        }
+
+        public override void damage()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+
+
+    }
+}
